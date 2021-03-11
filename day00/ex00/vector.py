7d56a831ef3abed62444333ef185fe1bfa04d980
Eredion <alvaro.segovia@hotmail.com>
2021-03-11 20:07:53 +0100

    Go go go

--- day00/ex00/vector.py
+++ day00/ex00/vector.py
@@ -1,18 +1,33 @@
 #! /usr/bin/env python3
 
-print(type(5))
-print(type((1, 3)))
-print(type([1, 2, 3]))
-
-
 class Vector:
-    def __init__(self, arg):
-        if isinstance(arg, int):
-            self.size = arg
-            self.values = range(0, (arg))
+	def __init__(self, arg):
+		try:
+			self.values = []
+			if isinstance(arg, int):
+				self.size = arg
+				for i in range(arg):
+					self.values.append(float(i))
+			elif isinstance(arg, list):
+				self.size = len(arg)
+				for i in arg:
+					if isinstance(i, float) == False:
+						raise(ValueError)
+				self.values = arg
+			elif isinstance(arg, tuple) and len(arg) == 2:
+				for i in range(arg[0], arg[1]):
+					self.values.append(float(i))
+				self.size = len(self.values)
+			else:
+				raise(ValueError)
+		except ValueError:
+			exit("Error: Wrong vector initialization")
 
-    def __str__(self):
-        return (f"Vector size: {self.size}\nVector values: {self.values}\n")
+	def __str__(self):
+		return (f"Vector values: {self.values}")
+
+	def __repr__(self):
+		return (f"Vector size: {self.size}\nVector values: {self.values}")
 """
     def __add__
 
@@ -35,5 +50,8 @@ class Vector:
     def __repr__
 """
 
-v = Vector(3)
-print(v)
+
+print(Vector(8))
+print(Vector([1.0, 2.3, 4.5]))
+print(Vector((10,15)))
+
