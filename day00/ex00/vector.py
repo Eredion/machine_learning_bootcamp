502108c55e1eecfcac45b32f8163c3b69d56bb57
Eredion <alvaro.segovia@hotmail.com>
2021-03-12 12:48:50 +0100

    .

--- day00/ex00/vector.py
+++ day00/ex00/vector.py
@@ -1,33 +1,38 @@
 #! /usr/bin/env python3
 
+import numpy as np
+
+
 class Vector:
-	def __init__(self, arg):
-		try:
-			self.values = []
-			if isinstance(arg, int):
-				self.size = arg
-				for i in range(arg):
-					self.values.append(float(i))
-			elif isinstance(arg, list):
-				self.size = len(arg)
-				for i in arg:
-					if isinstance(i, float) == False:
-						raise(ValueError)
-				self.values = arg
-			elif isinstance(arg, tuple) and len(arg) == 2:
-				for i in range(arg[0], arg[1]):
-					self.values.append(float(i))
-				self.size = len(self.values)
-			else:
-				raise(ValueError)
-		except ValueError:
-			exit("Error: Wrong vector initialization")
+    def __init__(self, arg):
+        try:
+            self.values = []
+            if isinstance(arg, int):
+                self.size = arg
+                for i in range(arg):
+                    self.values.append(float(i))
+            elif isinstance(arg, list):
+                self.size = len(arg)
+                for i in arg:
+                    if isinstance(i, float) is False:
+                        raise(ValueError)
+                    self.values = arg
+            elif isinstance(arg, tuple) and len(arg) == 2:
+                for i in range(arg[0], arg[1]):
+                            self.values.append(float(i))
+                            self.size = len(self.values)
+            else:
+                raise(ValueError)
+        except ValueError:
+            exit("Error: Wrong vector initialization")
 
-	def __str__(self):
-		return (f"Vector values: {self.values}")
+    def __str__(self):
+        return (f"Vector values: {self.values}")
 
-	def __repr__(self):
-		return (f"Vector size: {self.size}\nVector values: {self.values}")
+    def __repr__(self):
+        return (f"Vector size: {self.size}\nVector values: {self.values}")
+
+
 """
     def __add__
 
@@ -54,4 +59,7 @@ class Vector:
 print(Vector(8))
 print(Vector([1.0, 2.3, 4.5]))
 print(Vector((10,15)))
-
+print(type(Vector(8)))
+a = np.ones(5)
+print(a)
+print(a + 5)
---
+++ day00/ex03/prediction.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+import numpy as np
+
+x = np.arange(1, 6)
+print(x)
+y = np.array(([5, 0], [1, 2]))
+print(y)
+print(y.shape)
+#def simple_predict(x, theta):
