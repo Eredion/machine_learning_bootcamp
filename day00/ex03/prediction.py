82c163fb3d1c995f59d0d2ecec60b3cfe41b3e68
Eredion <alvaro.segovia@hotmail.com>
2021-03-15 09:18:08 +0100

    Finished ex03

--- day00/ex03/prediction.py
+++ day00/ex03/prediction.py
@@ -2,9 +2,27 @@
 
 import numpy as np
 
-x = np.arange(1, 6)
-print(x)
-y = np.array(([5, 0], [1, 2]))
-print(y)
-print(y.shape)
-#def simple_predict(x, theta):
+
+def simple_predict(x, theta):
+    if type(theta) != np.ndarray or type(x) != np.ndarray or x.ndim != 1\
+            or theta.ndim != 1 or len(theta) != 2:
+        return None
+    else:
+        y_hat = []
+        for i in x:
+            y_hat.append(theta[0] + i * theta[1])
+        return (y_hat)
+
+if __name__ == "__main__":
+    x = np.arange(1, 6)
+    theta1 = np.array([5, 0])
+    print(simple_predict(x, theta1))
+
+    theta2 = np.array([0, 1])
+    print(simple_predict(x, theta2))
+
+    theta3 = np.array([5, 3])
+    print(simple_predict(x, theta3))
+
+    theta4 = np.array([-3, 1])
+    print(simple_predict(x, theta4))
