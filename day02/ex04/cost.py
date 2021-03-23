24d663bb868dcdb15eea8beda7de433fe24c754d
Eredion <alvaro.segovia@hotmail.com>
2021-03-23 12:37:55 +0100

    adding cost

---
+++ day02/ex04/cost.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def cost_(y, y_hat):
+    if type(y_hat) != np.ndarray or type(y) != np.ndarray or y.ndim != 1\
+            or y_hat.ndim != 1 or len(y_hat) != len(y):
+        return None
+    else:
+        return(np.dot((1 / (len(y)))*(y_hat - y), (y_hat - y)))
+
+if __name__ == "__main__":
+    X = np.array([0, 15, -9, 7, 12, 3, -21])
+    Y = np.array([2, 14, -13, 5, 12, 4, -19])
+    print(cost_(X, Y))
+    print(cost_(X, X))
